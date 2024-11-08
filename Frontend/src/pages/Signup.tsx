5735b8bfc3aa67173e53256ec81a30aaf664288e
Astitva Nigam <nigamastitva001@gmail.com>
2024-11-08 21:50:04 +0530

    init

---
+++ Frontend/src/pages/Signup.tsx
@@ -0,0 +1,14 @@
+import { Quote } from "../components/quotes";
+import { Auth } from "../components/auth";
+export const Signup = () => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2">
+      <div>
+        <Auth type="signup" />
+      </div>
+      <div className="hidden lg:block">
+        <Quote />
+      </div>
+    </div>
+  );
+};
